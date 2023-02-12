afd67850dc5c98619e94fd453e7b86aabb6cbde7
JacobLinCool <JacobLinCool@users.noreply.github.com>
2023-02-12 00:50:45 +0000

    deploy: 572c71c28a81c1464b40677b61941e19ed18cbac

---
+++ _app/immutable/chunks/2-61bdac2c.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-8c1d5f30.js";export{t as component};
--- _app/immutable/chunks/2-76ee4c42.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-0d883024.js";export{t as component};
